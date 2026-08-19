5e9ffb340cf7d390d61aaf8d5efa8945e5f2daae
BtrixKiddo <43914147+BtrixKiddo@users.noreply.github.com>
2026-08-19 16:48:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fichaje-v10';
+const CACHE = 'fichaje-v11';
 const ASSETS = ['.', 'index.html', 'styles.css', 'app.js', 'manifest.webmanifest', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
